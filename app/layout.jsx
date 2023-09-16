d5dad8d8baf6408d3ede69ef2cafc255ca250f07
Akbar Afriansyah <itsme@akbarafriansyah.my.id>
2023-09-16 16:36:57 +0700

    setup home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,8 @@
 import '@styles/globals.css'
-import { children } from 'react'
 
 export const metadata = {
     title : "Next wedding",
-    description: "Aplikasi wedding 2.0"
+    description: "Aplikasi wedding 2.0",
 }
 
 const RootLayout = ({ children}) => {
@@ -13,11 +12,11 @@ const RootLayout = ({ children}) => {
             <div className="main">
                 <div className="gradient" />
             </div>
-        </body>
 
-        <main className="app">
-            {children}
-        </main>
+            <main className="app">
+                {children}
+            </main>
+        </body>
     </html>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,16 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & share wedding party on
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> NextWedding App</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus maiores modi reiciendis. Neque obcaecati nulla libero reprehenderit ullam, aperiam debitis. Reprehenderit dolorum ea nostrum, esse quia sequi dicta soluta illo?
+      </p>
+    </section>
   )
 }
 
